477b816feb0c0210a19aae9548be7f4ceb34bd58
lau05avend <lvavendano6@misena.edu.co>
2021-12-09 19:58:44 -0500

    Actualizacion en proyecto heroku

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(RoleSeeder::class);
         $this->call(PermissionsSeeder::class);
         $this->call(PermissionsToRolesSeeder::class);
+        $this->call(AdminSeeder::class);
     }
 }
